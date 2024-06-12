93537fdb1e65ce8bc04883ac275be394c5033749
Inf0 <106565371+Inf0sth@users.noreply.github.com>
2024-06-12 15:36:31 +0000

    On branch main Changes to be committed: 	modified:   Exam_1/main.java

--- Exam_1/main.java
+++ Exam_1/main.java
@@ -7,10 +7,51 @@ public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Chose your shape type: ");
-        System.out.println("Ellipse: E");
-        System.out.println("Triangle: T");
-        System.out.println("Quadrilateral: Q");
+        System.out.println("Ellipse: 1");
+        System.out.println("Triangle: 2");
+        System.out.println("Quadrilateral: 3");
+        System.out.println("Select option: ");
         int option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                Ellipse shape = new Ellipse();
+                System.out.println("Area: 1");
+                System.out.println("Perimeter: 2");
+                System.out.println("Select: ");
+                int cal = scanner.nextInt();
+                switch (cal) {
+                    case 1:
+                        System.out.println("Radio Y:");
+                        int Y = scanner.nextInt();
+                        System.out.println("Radio X ");
+                        int X = scanner.nextInt();
+                        shape.calArea(Y, X);
+                        System.out.println(shape.getArea());
+                        break;
+                    case 2:
+                        System.out.println("Radio Y:");
+                        int Y = scanner.nextInt();
+                        System.out.println("Radio X ");
+                        int X = scanner.nextInt();
+                        shape.calPerimeter(Y, X);
+                        System.out.println(shape.getPerimeter());
+                        break;
+                    default:
+                        break;
+                }
+
+
+                break;
+
+            case 2:
+                break;
+        
+
+            case 3:
+                break;
+            default:
+                break;
+        }
 
     }
 }
